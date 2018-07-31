15c05fc966d11ccee8516e8ec56a0e313cbde7c3
Yuliang Liu <yuliang.liu@yuliang-l-mbp.centrify.com>
2018-07-31 11:39:17 -0700

    add max-old-space-size parameter

--- src/renderer.js
+++ src/renderer.js
@@ -198,7 +198,7 @@ class Renderer {
 
 
 async function create() {
-  const browser = await puppeteer.launch({ executablePath: 'google-chrome-unstable',args: ['--no-sandbox','--disable-dev-shm-usage','--media-cache-size=1','--disk-cache-size=1','--disable-application-cache','--disable-session-storage','--js-flags="--max-old-space-size=200"']})
+  const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-dev-shm-usage','--media-cache-size=1','--disk-cache-size=1','--disable-application-cache','--disable-session-storage','--js-flags="--max-old-space-size=200"']})
   return new Renderer(browser)
 }
 
